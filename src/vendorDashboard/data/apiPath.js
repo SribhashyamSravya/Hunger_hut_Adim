143ae7b8fabf52c1245debbd69af0896e6cb8717
Sribhashyam Sravya <ssravya2004@gmail.com>
2025-04-08 13:52:53 +0530

    commit

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://localhost:5000/api";
+//export const API_URL = "http://localhost:5000/api";
 
 
 
-//export const API_URL = "https://q-backend-510k.onrender.com"
+export const API_URL = "https://hungerhut-backend.onrender.com"
